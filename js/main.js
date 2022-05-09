7cb46dfcdb49b84dfc91d3f1f2cc55531b3fc21e
lutfiqaraman <lutfiqaraman@gmail.com>
2022-05-09 22:06:49 +0300

    build a line chart using d3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+const pathOfCSVFile = "../data/stockdata.csv";
+
 d3
-    .csv("../data/stockdata.csv", d => {
+    .csv(pathOfCSVFile, d => {
         return {
             date: d["Date"],
             high: d["High"],
@@ -20,5 +22,21 @@ const svg = d3.select(".fins-diagram-container")
         .style("border", "1px solid black");
 
 const CreateLineChart = (data) => {
-    // your diagram
+    let yearsOfTrading = getYearsOfTrading(data);
+}
+
+const getYearsOfTrading = (data) => {
+    let years   = [];
+
+    const dateParse  = d3.timeParse("%d/%m/%Y");
+    const dateFormat = d3.timeFormat("%Y");
+
+    for (let obj of data) {
+        const yearsOfTrading = dateFormat(dateParse(obj.date));
+        if(years.indexOf(yearsOfTrading) === -1) {
+            years.push(yearsOfTrading);
+        }
+    }
+
+    return years;
 }
