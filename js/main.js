040b1c146144d0d3da392e85aac5396d482d600f
lutfiqaraman <lutfiqaraman@gmail.com>
2022-05-13 21:09:28 +0300

    build a line chart using d3

--- js/main.js
+++ js/main.js
@@ -16,25 +16,15 @@ d3
         CreateLineChart(data);
     });
 
-const svg = d3.select(".fins-diagram-container")
+const svg = d3.select(".fins_line_chart_container")
     .append("svg")
-        .attr("viewBox", "0 0 600 700");
+        .attr("viewBox", "0 0 960 200")
+        .style("border", "1px solid #000")
 
 const CreateLineChart = (data) => {
     const yearsOfTrading = getYearsOfTrading(data);
     const domain = d3.extent(yearsOfTrading);
 
-    let scale = d3.scaleLinear()
-        .domain(domain)
-        .range([0, 200]);
-
-    // Add scales to axis
-    let x_axis = d3.axisBottom()
-        .scale(scale);
-
-    //Append group and insert axis
-    svg.append("g")
-        .call(x_axis);
 }
 
 const getYearsOfTrading = (data) => {
